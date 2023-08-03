a7383f5181b3559c0c40f6e3c3797ebe7d6c53b6
Kanimet <kanimet200127@gmail.com>
2023-08-03 12:56:41 +0300

    Fixed a bug: Changed api resource Product.

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Attribute;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\ProductContent;
@@ -28,9 +29,9 @@ class ProductResource extends BaseResource
             'category' => $this->category($this->resource->category),
             'name' => $content->name,
             'description' => $content->description,
-            'is_available' => (bool) $this->resource->is_available,
+            'is_available' => (bool)$this->resource->is_available,
             'images' => getImages($this->resource->images->values),
-            'properties' => $this->getProperties($this->resource->properties),
+            'attributes' => $this->getAttributes($this->resource->productAttributes ?? []),
             'selections' => $this->getSelections($this->resource->selections),
             'preview' => $this->resource->preview,
         ];
@@ -49,20 +50,29 @@ class ProductResource extends BaseResource
         return null;
     }
 
-    protected function getProperties(array $properties): array
+    public function getAttributes(Collection|array $attributes): array
     {
         $locale = self::$locale;
-
         $array = [];
 
-        foreach ($properties as $property) {
+        /** @var Attribute $attribute */
+        foreach ($attributes as $attribute) {
             $array[] = [
-                'name' => $property['name'][$locale],
-                'properties' => $property['properties'][$locale],
+                'attribute' => $attribute->attribute,
+                'name' => __('common.attributes.' . $attribute->attribute),
+                'value' => $this->getAttributeValue($attribute->type, $attribute->{'value' . $attribute->type}, $locale)
             ];
         }
 
         return $array;
+    }
+
+    protected function getAttributeValue(int $type, array|string $value, string $locale)
+    {
+        return match ($type) {
+            1 => $value[$locale],
+            default => $value
+        };
     }
 
     protected function getSelections(Collection $selections): array
