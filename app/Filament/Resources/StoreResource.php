1bca92fd05ea6beebe6ab3fc318ca912d9edce0d
Kanimet <kanimet200127@gmail.com>
2023-08-14 09:26:23 +0300

    Made a role restriction in Store management

--- app/Filament/Resources/StoreResource.php
+++ app/Filament/Resources/StoreResource.php
@@ -3,16 +3,8 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\StoreResource\Pages;
-use App\Helpers\FilamentHelper;
-use App\Models\Category;
 use App\Models\Store;
-use App\Models\User;
-use Closure;
-use Exception;
-use Filament\Resources\Form;
 use Filament\Resources\Resource;
-use Filament\Resources\Table;
-use Filament\Tables;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
@@ -27,54 +19,14 @@ class StoreResource extends Resource
     {
         $user = Auth::user();
 
-        if ($user->hasRole('admin')) {
-            return parent::getEloquentQuery();
+        if ($user->hasRole('store_manager')) {
+            return parent::getEloquentQuery()->whereIn('id', $user->stores_permission);
+        } else if ($user->hasRole('store_owner')) {
+            return parent::getEloquentQuery()->where('user_id', $user->id);
         }
 
-        return parent::getEloquentQuery()->where('user_id', $user->id);
-    }
+        return parent::getEloquentQuery();
 
-    public static function form(Form $form): Form
-    {
-        $helper = new FilamentHelper;
-        $locale = config('app.locale');
-        $locales = config('app.locales');
-        $categories = Category::all()->pluck("name.$locale", 'id');
-
-        return $form
-            ->schema([
-                $helper->select('user_id')
-                    ->options(User::pluck('name', 'id'))
-                    ->label('Store Owner')
-                    ->required(),
-                $helper->input('name'),
-                $helper->grid([
-                    $helper->checkbox('locales', $locales)
-                        ->reactive()
-                        ->required()
-                        ->columns(count($locales)),
-                    $helper->radio(
-                        'fallback_locale',
-                        fn(Closure $get) => filterAvailableLocales($get('locales'))
-                    )->hidden(fn(Closure $get) => !count($get('locales')))
-                        ->required(fn(Closure $get) => count($get('locales')))
-                        ->inline(),
-                    $helper->checkbox('categories', $categories)
-                        ->required()
-                        ->columns()
-                        ->dehydrateStateUsing(function ($state) {
-                            $array = [];
-
-                            foreach ($state as $value) {
-                                $array[] = (int)$value;
-                            }
-
-                            return Category::whereIn('id', $array)
-                                ->pluck('id')
-                                ->toArray();
-                        })
-                ], 1),
-            ])->columns(2);
     }
 
     public static function canCreate(): bool
@@ -92,35 +44,6 @@ class StoreResource extends Resource
     public static function canDelete(Model $record): bool
     {
         return Auth::user()->hasRole('admin');
-    }
-
-    /**
-     * @throws Exception
-     */
-    public static function table(Table $table): Table
-    {
-        $columns = [
-            Tables\Columns\TextColumn::make('name'),
-        ];
-
-        if (Auth::user()->hasRole('admin')) {
-            $columns[] = Tables\Columns\TextColumn::make('uuid')
-                ->copyable();
-            $columns[] = Tables\Columns\TextColumn::make('user.name');
-        }
-
-        $columns[] = Tables\Columns\TextColumn::make('updated_at');
-
-        return $table
-            ->columns($columns)
-            ->actions([
-                Tables\Actions\Action::make('Add product')
-                    ->url(fn(Model $record) => route('filament.resources.products.create', [
-                        'store_id' => $record->id
-                    ])),
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([]);
     }
 
     public static function getPages(): array
--- app/Filament/Resources/StoreResource/Pages/CreateStore.php
+++ app/Filament/Resources/StoreResource/Pages/CreateStore.php
@@ -3,9 +3,34 @@
 namespace App\Filament\Resources\StoreResource\Pages;
 
 use App\Filament\Resources\StoreResource;
+use App\Models\Category;
+use App\Models\User;
+use Filament\Resources\Form;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Collection;
 
 class CreateStore extends CreateRecord
 {
     protected static string $resource = StoreResource::class;
+
+    public array|Collection $categories = [];
+    public array|Collection $users = [];
+
+    public function mount(): void
+    {
+        $this->categories = Category::all()->pluck('name.' . config('app.locale'), 'id');
+        $this->users = User::where('role_id', 2)->pluck('name', 'id');
+
+        parent::mount();
+    }
+
+    protected function form(Form $form): Form
+    {
+        $resourceForm = new StoreResource\StoreResourceForm(
+            $this->categories,
+            $this->users,
+        );
+
+        return $resourceForm->form($form);
+    }
 }
