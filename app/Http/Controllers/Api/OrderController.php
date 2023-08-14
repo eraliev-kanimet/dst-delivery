db9b0ccc3e55ce2167a9f2342a43af5beeddd0f1
Kanimet <kanimet200127@gmail.com>
2023-08-14 10:28:24 +0300

    Wrote an api to get info about order statuses, supported payment types and shipping types

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\DeliveryType;
+use App\Enums\OrderStatus;
+use App\Enums\PaymentType;
 use App\Http\Controllers\Controller;
 
 class OrderController extends Controller
 {
     public function info()
-    {}
+    {
+        $statuses = [];
+
+        foreach (OrderStatus::cases() as $status) {
+            $statuses[] = [
+                'key' => $status->value,
+                'name' => __('common.order_status.' . $status->name)
+            ];
+        }
+
+        $payment_types = [];
+
+        foreach (PaymentType::cases() as $type) {
+            $payment_types[] = [
+                'key' => $type->value,
+                'name' => __('common.payment_types.' . $type->name)
+            ];
+        }
+
+        $delivery_types = [];
+
+        foreach (DeliveryType::cases() as $type) {
+            $delivery_types[] = [
+                'key' => $type->value,
+                'name' => __('common.delivery_types.' . $type->name)
+            ];
+        }
+
+        return response()->json([
+            'statuses' => $statuses,
+            'payment_types' => $payment_types,
+            'delivery_types' => $delivery_types,
+        ]);
+    }
 }
